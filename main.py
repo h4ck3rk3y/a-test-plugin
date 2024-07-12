11b121104991e0cff263252407f741346630da4a
Gyanendra Mishra <anomaly.the@gmail.com>
2024-07-12 12:48:59 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+REPLACED = "the-text-has-been-replaced""
+
+def setup(manifest, service_name, text_to_replace):
+  manifest = manifest.replace(text_to_replace, REPLACED)
+  return manifest
+
+def teardown(manifest, service_name):
+  manifest = manifest.replace(REPLACED, "not-any-more")
+  return manifest
