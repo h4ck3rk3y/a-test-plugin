468cf6000e358eed32f46c235d4ae7e30cf7271c
Gyanendra Mishra <anomaly.the@gmail.com>
2024-07-15 14:56:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import json
+
 REPLACED = "the-text-has-been-replaced"
 
-def setup(manifest, service_name, text_to_replace):
-  manifest = manifest.replace(text_to_replace, REPLACED)
-  return manifest
+def create_flow(service_spec, deployment_spec, flow_uuid, text_to_replace):
+    deployment_spec['Template']['Labels']['app'] = deployment_spec['Template']['Labels']['app'].replace(text_to_replace, REPLACED)
+    deployment_spec['Selector']['MatchLabels']['app'] = deployment_spec['Selector']['MatchLabels']['app'].replace(text_to_replace, REPLACED)
+    deployment_spec['Template']['Spec']['Containers'][0]['Name'] = deployment_spec['Template']['Spec']['Containers'][0]['Name'].replace(text_to_replace, REPLACED)
+    
+    config_map = {
+        "original_text": text_to_replace
+    }
+    
+    return {
+        "deployment_spec": deployment_spec,
+        "config_map": json.dumps(config_map)
+    }
 
-def teardown(manifest, service_name):
-  manifest = manifest.replace(REPLACED, "not-any-more")
-  return manifest
+def delete_flow(config_map, flow_uuid):
+    return None
