5363dbf44ce6b11b9d6b13fde05b8142c79ab212
Gyanendra Mishra <anomaly.the@gmail.com>
2024-07-15 15:08:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import json
 REPLACED = "the-text-has-been-replaced"
 
 def create_flow(service_spec, deployment_spec, flow_uuid, text_to_replace):
-    deployment_spec = json.loads(deployment_spec)
-    
     deployment_spec['template']['labels']['app'] = deployment_spec['template']['labels']['app'].replace(text_to_replace, REPLACED)
     deployment_spec['selector']['matchLabels']['app'] = deployment_spec['selector']['matchLabels']['app'].replace(text_to_replace, REPLACED)
     deployment_spec['template']['spec']['containers'][0]['name'] = deployment_spec['template']['spec']['containers'][0]['Name'].replace(text_to_replace, REPLACED)
